0a2851e7a2c18fa7614490ef15b434f4aef04a46
Vítor Ribeiro <168368349+vitor3g@users.noreply.github.com>
2024-12-24 11:05:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-const API_BASE_URL = "http://168.90.5.62:8080/admin";
+const API_BASE_URL = "http://168.90.5.62:8080";
 
 app.use(express.raw({ type: "*/*" }));
 
